c315798984a7da3a5599174dc825d4d3fa553f67
Mannat Garg <mannatgarg2006@gmail.com>
2025-02-18 17:03:49 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
 const port = 3010;
+const env = process.env.MONGO_URI;
+
+mongoose.connect(env).then(()=>{
+  console.log("Connected to database..!")
+})
+.catch(()=>{
+  console.log("Connection failed..!")
+})
+
 
 app.use(express.static('static'));
 
@@ -11,5 +21,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
+  
   console.log(`Example app listening at http://localhost:${port}`);
 });
